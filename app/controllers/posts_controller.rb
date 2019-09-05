535546297aae06d76e5f3fab9182b63139d58164
aparkening <aaron.parkening@gmail.com>
2019-09-05 12:01:21 -0400

    Done. Create validations, routes, controllers, and forms with errors.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,35 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    render :edit
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.errors.any?
+      render :edit
+    else
+      redirect_to post_path(@post)
+    end
   end
 
   private
@@ -20,4 +37,9 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:title, :category, :content)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
