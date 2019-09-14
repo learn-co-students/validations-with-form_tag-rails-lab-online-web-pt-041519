53fb0f6afcf5e8468318e4156d5a33be06de7a6c
Meg Galvez <meg@oakmaine.com>
2019-09-13 20:09:39 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
-end
+    validates :title, presence: true 
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+end 
